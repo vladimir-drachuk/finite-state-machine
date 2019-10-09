33904b239e3e9dedb4536b12873e2f3ab07bdd12
vladimir-drachuk <vvdra4@gmail.com>
2019-10-10 00:19:39 +0300

    feat: add task

--- src/fsm.js
+++ src/fsm.js
@@ -1,61 +1,138 @@
 class FSM {
-    /**
-     * Creates new FSM instance.
-     * @param config
-     */
-    constructor(config) {}
 
-    /**
-     * Returns active state.
-     * @returns {String}
-     */
-    getState() {}
+    constructor(config) {
+        this.state = config;
+        this.status = this.state.initial;
+        this.und = '';
+        this.history = ['normal'];
+        this.historyState = 1;
+    }
 
-    /**
-     * Goes to specified state.
-     * @param state
-     */
-    changeState(state) {}
+    getState() {
+        return this.status
+    }
 
-    /**
-     * Changes state according to event transition rules.
-     * @param event
-     */
-    trigger(event) {}
+    changeState(state) {
+        for (let key in this.state.states) {
+            if (key === state) {
+                this.status = state;
+                if (this.history.length > this.historyState) {
+                    for (let i = 0; i <= this.history.length - this.historyState; i++) {
+                    this.history.pop()
+                    }
+                }
+                this.history.push(state);
+                this.historyState++;
+                console.log(this.history);
+                return;
+            }
+        }
+        throw new Error();
+    }
 
-    /**
-     * Resets FSM state to initial.
-     */
-    reset() {}
+    trigger(event) {
+         for (let key in this.state.states[this.status].transitions) {
+             if (key === event) {
+                 this.status = this.state.states[this.status].transitions[event];
+                 if (this.history.length > this.historyState) {
+                     for (let i = 0; i <= this.history.length - this.historyState; i++) {
+                     this.history.pop()
+                     }
+                 }
+                 this.history.push(this.status);
+                 this.historyState++;
+                 console.log(this.history);
+                 return;
+             }
+         }
+         throw new Error();
+    }
 
-    /**
-     * Returns an array of states for which there are specified event transition rules.
-     * Returns all states if argument is undefined.
-     * @param event
-     * @returns {Array}
-     */
-    getStates(event) {}
+    reset() {
+        this.status = 'normal';
+    }
 
-    /**
-     * Goes back to previous state.
-     * Returns false if undo is not available.
-     * @returns {Boolean}
-     */
-    undo() {}
+    getStates(event) {
+        let array = [];
+        if (event != 'get_up' && event != 'get_tired' && event != 'get_hungry' && event != 'eat' && event != 'study' && event !== undefined) return array;
+        for (let key in this.state.states.normal.transitions) {
+            if (key === event) {
+                array.push('normal');
+            }
+        }    
+        for (let key in this.state.states.busy.transitions) {
+            if (key === event) {
+                array.push('busy');
+            }
+        }
+        for (let key in this.state.states.hungry.transitions) {
+            if (key === event) {
+                array.push('hungry');
+            }
+        }
+        for (let key in this.state.states.sleeping.transitions) {
+            if (key === event) {
+                array.push('sleeping');
+            }
+        }
+        if (array.length === 0) {array = ['normal', 'busy', 'hungry', 'sleeping']} 
+        return array;
 
-    /**
-     * Goes redo to state.
-     * Returns false if redo is not available.
-     * @returns {Boolean}
-     */
-    redo() {}
+    }
 
-    /**
-     * Clears transition history
-     */
-    clearHistory() {}
+    undo() {
+        let different = this.history.length - this.historyState + 1;
+        if (this.historyState === 1) return false;
+        this.status = this.history[this.history.length - different - 1];
+        this.historyState--;
+        return true;
+
+    }
+
+    redo() {
+        let different = this.history.length - this.historyState;
+        if (different === 0) return false;
+        this.status = this.history[this.history.length - different];
+        this.historyState++;
+        if (different === 0) return false;
+        return true;
+
+
+    }
+
+    clearHistory() {
+        this.history = ['normal'];
+        this.historyState = 1;
+    }
 }
+
+const config = {
+    initial: 'normal',
+    states: {
+        normal: {
+            transitions: {
+                study: 'busy',
+            }
+        },
+        busy: {
+            transitions: {
+                get_tired: 'sleeping',
+                get_hungry: 'hungry',
+            }
+        },
+        hungry: {
+            transitions: {
+                eat: 'normal'
+            },
+        },
+        sleeping: {
+            transitions: {
+                get_hungry: 'hungry',
+                get_up: 'normal',
+            },
+        },
+    }
+};
 
 module.exports = FSM;
 
-/** @Created by Uladzimir Halushka **/
